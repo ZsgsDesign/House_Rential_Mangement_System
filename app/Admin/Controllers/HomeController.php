307652aad5c7a3d647873a15fc5ae97eaa139574
ZsgsDesign <zsgsdesign@gmail.com>
2020-12-23 09:20:39 +0800

    update

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -7,6 +7,11 @@ use Encore\Admin\Controllers\Dashboard;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Row;
+use App\Models\Area;
+use App\Models\Consumer;
+use App\Models\House;
+use App\Models\Property;
+use App\Models\Purchase;
 
 class HomeController extends Controller
 {
@@ -15,16 +20,23 @@ class HomeController extends Controller
         return $content
             ->title('仪表盘')
             ->description('这里是管理系统的系统运行状态区')
-            // ->row(Dashboard::title())
+            ->row(view('admin.dashboard.title'))
             ->row(function (Row $row) {
 
-                $row->column(8, function (Column $column) {
-                    $column->append(Dashboard::environment());
+                $row->column(4, function (Column $column) {
+                    $envs=[
+                        ['name' => '地区总数', 'value' => Area::count()],
+                        ['name' => '物业总数', 'value' => Property::count()],
+                        ['name' => '房源总数', 'value' => House::count()],
+                        ['name' => '客户总数', 'value' => Consumer::count()],
+                        ['name' => '订单总数', 'value' => Purchase::count()],
+                    ];
+                    $column->append(view('admin.dashboard.stat', compact('envs')));
                 });
 
-                // $row->column(4, function (Column $column) {
-                //     $column->append(Dashboard::extensions());
-                // });
+                $row->column(4, function (Column $column) {
+                    $column->append(Dashboard::environment());
+                });
 
                 $row->column(4, function (Column $column) {
                     $column->append(Dashboard::dependencies());
